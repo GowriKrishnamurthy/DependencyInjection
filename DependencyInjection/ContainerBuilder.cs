632495ec8a3bc1d95a7b569ab16d6740561cae1e
Gowri Krishnamurthy <kr_gowri@yahoo.com>
2020-09-19 15:48:25 +1000

    Dependency injection builder

---
+++ DependencyInjection/ContainerBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjection
+{
+    public class ContainerBuilder
+    {
+        public IServiceProvider Build()
+        {
+            var container = new ServiceCollection()
+                .AddSingleton<IOrderManger, OrderManger>()
+                .AddSingleton<IPaymentService, PaymentService>()
+                .AddSingleton<IProductStockRepository, ProductStockRepository>()
+                .AddSingleton<IShippingService, ShippingService>();
+
+            return container.BuildServiceProvider();
+        }
+    }
+}
